b238ba73ba9fb54d42c94f8e4ab534d299487f47
TAYEGGHD <103031475+TAYEGGHD@users.noreply.github.com>
2022-04-08 16:22:06 +0100

    Add files via upload

---
+++ Act-1.4/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+public static void main(String[] args) {
+Bateau bateau = new Bateau();
+Coque coque = new Coque();
+Pont pont = new Pont();
+Cabine cabine = new Cabine();
+Mat mat = new Mat();
+
+}
+}

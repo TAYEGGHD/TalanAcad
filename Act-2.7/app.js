3a6e73cdf41a4c935c930098ddc0e07fc9d4bddd
TAYEGGHD <103031475+TAYEGGHD@users.noreply.github.com>
2022-04-26 14:34:05 +0000

    Add files via upload

---
+++ Act-2.7/app.js
@@ -0,0 +1,13 @@
+
+let counter =1;
+setInterval(() => {
+document.querySelector('h1').innerText = "Jour";
+
+counter++;
+if(counter >5)
+{document.querySelector('h1').innerText = "Nuit";
+document.querySelector('h1').style.color = "white";
+document.body.style.backgroundColor= "black";
+
+}
+}, 1000);

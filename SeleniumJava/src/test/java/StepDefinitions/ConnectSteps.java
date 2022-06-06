30231adfc57d32bb73967a89aa998bb57b5fcafb
TAYEGGHD <103031475+TAYEGGHD@users.noreply.github.com>
2022-06-06 15:50:27 +0000

    Add files via upload

---
+++ SeleniumJava/src/test/java/StepDefinitions/ConnectSteps.java
@@ -0,0 +1,47 @@
+package StepDefinitions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ConnectSteps {
+	WebDriver driver = null;
+	
+@SuppressWarnings("deprecation")
+@Given("user is on the login page of the site")
+public void user_is_on_the_login_page_of_the_site() {
+	System.out.println("user is on the login page of the site");
+	
+	
+	System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+	driver=new ChromeDriver();
+	
+	driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+	driver.manage().timeouts().pageLoadTimeout(40,TimeUnit.SECONDS);
+	
+	driver.navigate().to("https://demo.guru99.com/test/newtours/login.php");
+}
+
+@When("^user enters (.*) and (.*)$")
+public void user_enters_username_and_password (String username, String password) {
+	driver.findElement(By.name("userName")).sendKeys("username");
+	driver.findElement(By.name("password")).sendKeys("password");
+}
+
+@And("user clicks on submit")
+public void user_clicks_on_submit() {
+	driver.findElement(By.name("submit")).click();
+}
+
+@Then("user is navigated to the home page")
+public void user_is_navigated_to_the_home_page() {
+	System.out.println("user is navigated to the home page");
+}
+}
